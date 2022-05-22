fc78a011afb2a327d6f3401bf1f52af43c281d1e
Marcin Szymczyk <91828986+Marcin152@users.noreply.github.com>
2022-05-22 18:25:15 +0200

    Update main.tf

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -1,32 +1,32 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example-group"
-  location = "West Europe"
+resource "azurerm_resource_group" "rs" {
+  name     = var.name_r
+  location = var.location
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplesa"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_storage_account" "sa" {
+  name                     = var.name_sa
+  resource_group_name      = azurerm_resource_group.rs.name
+  location                 = azurerm_resource_group.rs.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 }
 
-resource "azurerm_service_plan" "example" {
-  name                = "example-service-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  os_type             = "Linux"
-  sku_name            = "S1"
+resource "azurerm_service_plan" "sp" {
+  name                = var.name_sp
+  location            = azurerm_resource_group.rs.location
+  resource_group_name = azurerm_resource_group.rs.name
+  os_type             = var.os_type
+  sku_name            = var.sku_name
 }
 
-resource "azurerm_linux_function_app" "example" {
-  name                = "example-function-app"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  service_plan_id     = azurerm_service_plan.example.id
+resource "azurerm_linux_function_app" "lfa" {
+  name                = var.name_lfa
+  location            = azurerm_resource_group.rs.location
+  resource_group_name = azurerm_resource_group.rs.name
+  service_plan_id     = azurerm_service_plan.sp.id
 
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  storage_account_name       = azurerm_storage_account.sa.name_sa
+  storage_account_access_key = azurerm_storage_account.sa.primary_access_key
 
   site_config {
     application_stack {
@@ -35,10 +35,10 @@ resource "azurerm_linux_function_app" "example" {
   }
 }
 
-resource "azurerm_function_app_function" "example" {
+resource "azurerm_function_app_function" "faf" {
   name            = "example-function-app-function"
-  function_app_id = azurerm_linux_function_app.example.id
-  language        = "Python"
+  function_app_id = azurerm_linux_function_app.lfa.id
+  language        = var.language
   test_data = jsonencode({
     "name" = "Azure"
   })
