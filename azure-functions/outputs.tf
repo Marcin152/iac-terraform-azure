0fbcd1d36f94fea9d6d06b306ff76616596e8d75
Marcin Szymczyk <91828986+Marcin152@users.noreply.github.com>
2022-05-22 23:18:23 +0200

    Update outputs.tf

--- azure-functions/outputs.tf
+++ azure-functions/outputs.tf
@@ -2,3 +2,9 @@ output "id" {
   description = "The function app ID"
   value       = azurerm_function_app_function.acr.id
 }
+
+output "primary_access_key" {
+  value       = azurerm_storage_account.sa_tf_state.primary_access_key
+  sensitive   = true
+  description = "Azure Storage Account - Primary access key"
+}
