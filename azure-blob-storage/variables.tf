500ada8326eefcd20d968e6b394285e67ba120e7
Marcin Szymczyk <91828986+Marcin152@users.noreply.github.com>
2022-05-22 23:10:57 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -1,1 +1,49 @@
+variable "name_rg" {
+  type = string
+  default = "resource-group"
+}
 
+variable "name_sa" {
+  type = string
+  default = "storage-account"
+}
+
+variable "name_sc" {
+  type = string
+  default = "storage-container"
+}
+
+variable "name_sb" {
+  type = string
+  default = "storage-blob"
+}
+
+variable "location" {
+  type = string
+  default = "West Europe"
+}
+
+variable "account_tier" {
+  type = string
+  default = "Standard"
+}
+
+variable "account_replication_type" {
+  type = string
+  default = "LRS"
+}
+
+variable "container_acces_type" {
+  type = string
+  default = "private"
+}
+
+variable "type" {
+  type = string
+  default = "Block"
+}
+
+variable "source" {
+  type = string
+  default = "local-file.zip"
+}
